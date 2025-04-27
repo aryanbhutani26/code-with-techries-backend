39ee0bc629ae8ffeff1f5a2723a5821e364298f8
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-04-27 12:42:36 +0530

    routes career

--- src/routes/careerRoutes.js
+++ src/routes/careerRoutes.js
@@ -3,19 +3,10 @@ import { createCareerController, getAllCareerController, getCareerByIdController
 
 const router = express.Router();
 
-// Create a job
 router.post("/", createCareerController);
-
-// Get all jobs
 router.get("/get", getAllCareerController);
-
-// Get job by ID
 router.get("/:id", getCareerByIdController);
-
-// Update a job by ID
 router.put("/update/:id", updateCareerByIdController);
-
-// Delete a job by ID
 router.delete("/delete/:id", deleteCareerByIdController);
 
 export default router;
