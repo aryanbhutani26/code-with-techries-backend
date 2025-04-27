6fbe2b13600e5c63b9918d236a475eee727e45f4
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-04-27 12:47:25 +0530

    change in config folder

--- config/index.js
+++ config/index.js
@@ -5,12 +5,6 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT||4000,
     DB_URL: process.env.MONGODB_URL,
-    SECRET: process.env.SESSION_SECRET,
-    google: {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    }
 }
 
 export default config;
