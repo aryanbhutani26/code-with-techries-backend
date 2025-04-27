cd4f8c81413e1985d7ded8dcb1988f3c31fa90f6
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-04-27 22:26:38 +0530

    add teacher auth to class routes

--- src/routes/classRoutes.js
+++ src/routes/classRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { protectAdmin } from "../middleware/adminAuth.js";
+import { protectTeacher } from "../middleware/teacherAuth.js";
 import { createNewClass, deleteExistingClass, getAllClasses, updateExistingClass } from '../controller/classController.js';
 
 const router = express.Router();
 
-router.post("/create", protectAdmin, createNewClass);
+router.post("/create", protectAdmin, protectTeacher, createNewClass);
 router.get("/all", getAllClasses);
-router.put("/update/:classId", protectAdmin, updateExistingClass);
-router.delete("/delete/:classId", protectAdmin, deleteExistingClass);
+router.put("/update/:classId", protectAdmin, protectTeacher, updateExistingClass);
+router.delete("/delete/:classId", protectAdmin, protectTeacher, deleteExistingClass);
 
 export default router;
